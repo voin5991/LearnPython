f40b80531913ed257632fb2ed765e05b7a55b845
voin5991 <76397893+voin5991@users.noreply.github.com>
2022-12-21 07:32:51 +0500

    Comment

--- nested_if.py
+++ nested_if.py
@@ -1,1 +1,13 @@
+x = int(input('x = '))
+
+if 0 < x < 7:                                   # Проверка на вхождение в диапозон.
+    print("Value is in range, let's continue")
+    y = 2 * x -5
+    if y < 0:
+        print("y is negative")
+    else:
+        if t > 0:
+            print("y is positive")
+        else:
+            print("y = 0")
 
