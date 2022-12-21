efb1701f9911554a9a2f355a5426aa39ef6934d5
voin5991 <76397893+voin5991@users.noreply.github.com>
2022-12-21 07:54:50 +0500

    condition2

--- Condition.py
+++ Condition.py
@@ -1,1 +1,8 @@
+is_ready = True
 
+if is_ready:
+    state_msg = "Ready"
+else:
+    state_msg = "Not ready yet"
+
+print(state_msg)
--- elif_switch.py
+++ elif_switch.py
@@ -1,1 +1,16 @@
+print("""Menu:
+1. File
+2. View
+3. Exit
+""")
+
+choice = input("Enter your choice: ")
+if choice == "1":
+    print("Ypu have selected 'File' menu")
+elif choice == "2":
+    print("You have selected 'View' menu")
+elif choice == "3":
+    print("Stopping")
+else:
+    print("Incorrect choice")
 
